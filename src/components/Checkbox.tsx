86355daae5db1499ecb3e2f00f0a3a7e3ba076fe
Matheus Chiari <mchiaridev@gmail.com>
2022-10-13 22:11:18 -0300

    ui: building form

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,12 +1,11 @@
-import { clsx } from "clsx";
 import { Check } from "phosphor-react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export const Checkbox = ({}: CheckboxProps) => {
+export const Checkbox = (props: CheckboxProps) => {
 	return (
-		<CheckboxPrimitive.Root className={"w-6 h-6 p-[2px] bg-gray-800 rounded"}>
+		<CheckboxPrimitive.Root className={"w-6 h-6 p-[2px] bg-gray-800 rounded"} {...props}>
 			<CheckboxPrimitive.Indicator asChild>
 				<Check weight='bold' className='h-5 w-5 text-cyan-500' />
 			</CheckboxPrimitive.Indicator>
